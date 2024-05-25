5618b8b54a9094c2c0edcd6a9e365fb61f0fec3c
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-05-25 11:34:55 -0500

    new migration to make cocktailingredient join table with amounts

---
+++ Migrations/20240525162535_dbUpdate.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CocktailClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientSavedCocktail");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DrinkId",
+                table: "SavedCocktails",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateTable(
+                name: "CocktailIngredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<string>(type: "text", nullable: false),
+                    IngredientId = table.Column<int>(type: "integer", nullable: false),
+                    SavedCocktailId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CocktailIngredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CocktailIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CocktailIngredients_SavedCocktails_SavedCocktailId",
+                        column: x => x.SavedCocktailId,
+                        principalTable: "SavedCocktails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CocktailIngredients_IngredientId",
+                table: "CocktailIngredients",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CocktailIngredients_SavedCocktailId",
+                table: "CocktailIngredients",
+                column: "SavedCocktailId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CocktailIngredients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DrinkId",
+                table: "SavedCocktails",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "IngredientSavedCocktail",
+                columns: table => new
+                {
+                    IngredientsId = table.Column<int>(type: "integer", nullable: false),
+                    SavedCocktailsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientSavedCocktail", x => new { x.IngredientsId, x.SavedCocktailsId });
+                    table.ForeignKey(
+                        name: "FK_IngredientSavedCocktail_Ingredients_IngredientsId",
+                        column: x => x.IngredientsId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientSavedCocktail_SavedCocktails_SavedCocktailsId",
+                        column: x => x.SavedCocktailsId,
+                        principalTable: "SavedCocktails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientSavedCocktail_SavedCocktailsId",
+                table: "IngredientSavedCocktail",
+                column: "SavedCocktailsId");
+        }
+    }
+}
---
+++ Models/CocktailIngredient.cs
@@ -0,0 +1,10 @@
+﻿namespace CocktailClub.Models
+{
+    public class CocktailIngredient
+    {
+        public int Id { get; set; }
+        public string Amount { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public SavedCocktail SavedCocktail { get; set; }
+    }
+}
