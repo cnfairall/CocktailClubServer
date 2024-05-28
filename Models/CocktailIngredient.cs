57ec3cd824d689c9c347fdcf80fe44788e42bce1
Courtney Fairall <101303134+cnfairall@users.noreply.github.com>
2024-05-28 16:34:42 -0500

    Update CocktailIngredient.cs

--- Models/CocktailIngredient.cs
+++ Models/CocktailIngredient.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public string Amount { get; set; }
+        public int IngredientId { get; set; }
         public Ingredient Ingredient { get; set; }
+        public int SavedCocktailId { get; set; }
         public SavedCocktail SavedCocktail { get; set; }
     }
 }
