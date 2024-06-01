9bc3cf9f99d0f2e12645875b120764133da2c848
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-06-01 10:06:51 -0500

    removed userid from review

--- Dtos/ReviewDto.cs
+++ Dtos/ReviewDto.cs
@@ -3,7 +3,6 @@
     public class ReviewDto
     {
         public int SavedCocktailId { get; set; }
-        public int UserId { get; set; }
         public string Notes { get; set; }
         public string Grade { get; set; }
     }
